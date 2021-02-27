023ac01820feb22352467bcfbe6c8f734e997f8a
Ryan Reabel <ryanreabel@gmail.com>
2021-02-27 17:10:19 -0500

    intial work with configuring UI and display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const defaultJSON = {
+  vertices: [
+    {
+      id: "n1",
+      label: "Node 1",
+      type: "node",
+    },
+    {
+      id: "n2",
+      label: "Node 2",
+      type: "node",
+    },
+    {
+      id: "a1",
+      label: "Alarm 1",
+      type: "alarm",
+    },
+  ],
+  edges: [
+    {
+      id: "e1",
+      label: "edge n1-n2",
+      type: "link",
+      source_id: "n1",
+      target_id: "n2",
+    },
+    {
+      id: "e2",
+      label: "edge n2-a1",
+      type: "link",
+      source_id: "n2",
+      target_id: "a1",
+    },
+  ],
+};
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <header className="App-header">Graph Generator</header>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>
+              <div className="jsonInput">
+                <TextField
+                  id="outlined-multiline-static"
+                  label="Multiline"
+                  multiline
+                  defaultValue={JSON.stringify(defaultJSON)}
+                  variant="outlined"
+                />
+              </div>
+            </Paper>
+            <Button variant="contained" color="primary">
+              Submit JSON
+            </Button>
+          </Grid>
+          <Grid item xs={8}>
+            <Paper className={classes.paper}>
+              <div className="graph">Something here</div>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
